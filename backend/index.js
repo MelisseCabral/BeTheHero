a6bb237d7c5426840693fc05c67ef3b54f96641c
Melisse Cabral <melissecabral@gmail.com>
2020-03-29 18:22:26 -0300

    add knext and driver sqlite3

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
+
+/**
+ * Rota / Recurso
+ */
+
+ /**
+  * Métodos HTTP
+  * GET: Buscar uma informação no Back End
+  * POST: Criar uma informação no Back End
+  * PUT: Alterar uma informção
+  * DELETE: Deletar uma informação
+  */
+
+  /**
+   * Tipos de Parametros
+   * 
+   * Query Params : Parâmetros nomeados enviados na rota após '?' (Filtros e Páginação)
+   * Route Params : Parâmetros utilizados para identificar recursos.
+   * Request Body : 
+   */
 
 app.get('/', (request, response) => {
-    return response.json({event:'Semana Oministack 11.0', student: 'Melisse'})
+    return request.send('Hello World');
 });
 
-app.listen(3333);
+app.post('/users', (request, response) => {
+    const params = request.body
+
+    console.log(params);
+    return response.json({event:'Semana Oministack 11.0', student: 'Mel'})
+});
+
+app.listen(3333); 
