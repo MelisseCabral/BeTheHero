6f142f65efdcd359393931da5b4cc7dd8d00c803
Melisse Cabral <melissecabral@gmail.com>
2020-04-02 12:42:02 -0300

    FrontEnd web Complete

---
+++ frontend/src/pages/NewIncident/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import {FiArrowLeft} from 'react-icons/fi'
+
+export default function NewIncident(){
+    return(
+        <div className="new-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+                    <h1>Cadastro Novo Caso</h1>
+                     <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso</p>
+                     <Link to="/" className='back-link'>
+                        <FiArrowLeft/>
+                        Voltar para Home
+                    </Link>
+                    
+                </section>
+
+                <form>
+                        <input placeholder="Titulo do Caso" type='text'/>
+                        <textarea placeholder='Descrição'/>
+                        <input placeholder="Valor em Reais"/>
+       
+                        <button type='submit' className="button">
+                            Cadastrar
+                        </button>
+                    </form>
+            </div>
+        </div>
+    );
+}  
